6636cea1eea1298683ee65b35a43af5f48951936
orforto <127221697+orforto@users.noreply.github.com>
2026-05-24 21:50:45 +0800

    替换公共代理为私有 CF Workers 节点

--- config/public-proxy.ts
+++ config/public-proxy.ts
@@ -1,20 +1,10 @@
 /**
- * 公共代理节点
+ * 私有代理节点
  */
 export const PUBLIC_PROXY_LIST: string[] = [
-  ...getDomainProxyList('worker-proxy.asia'),
-  ...getDomainProxyList('net-proxy.asia'),
-  ...getDomainProxyList('1235566.space'),
-  ...getDomainProxyList('worker-proxy.shop'),
-  ...getDomainProxyList('worker-proxys.cyou'),
-  ...getDomainProxyList('worker-proxy.cyou'),
+  'https://orforto.4fever.workers.dev',
+  'https://orforto1.4fever.workers.dev',
+  'https://orforto2.4fever.workers.dev',
+  'https://orforto3.4fever.workers.dev',
+  'https://orforto4.4fever.workers.dev',
 ];
-
-// 生成从00.到15.的16个二级域名
-function getDomainProxyList(domain: string): string[] {
-  const list: string[] = [];
-  for (let i = 0; i < 16; i++) {
-    list.push(`https://${('0' + i).slice(-2)}.${domain}`);
-  }
-  return list;
-}
